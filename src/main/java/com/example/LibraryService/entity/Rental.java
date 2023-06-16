8e3410c0a16a6b0789f27d49a60295694a1465cd
Yura <yura104bk@gmail.com>
2023-06-16 12:11:31 +0400

    Написано Rental, RentalController, RentalDTO, RentalRepository, RentalService

--- src/main/java/com/example/LibraryService/entity/Rental.java
+++ src/main/java/com/example/LibraryService/entity/Rental.java
@@ -3,9 +3,6 @@ package com.example.LibraryService.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @Entity
@@ -15,32 +12,14 @@ public class Rental {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_rental")
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "id_book")
-    private Book book;
-
-    @ManyToOne
-    @JoinColumn(name = "id_user")
-    private User user;
+    private Long id_rental;
 
     @Column(name = "dt_start_time")
-    private Date startTime;
+    private Date dt_start_time;
 
     @Column(name = "dt_end_time")
-    private Date endTime;
+    private Date dt_end_time;
 
     @Column(name = "overdue")
     private Integer overdue;
-
-    @Column(name = "is_active")
-    private boolean isActive;
-
-//    private long calculateDaysOverdue() {
-////        String formattedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-//        Date nowDate =
-//        int result = LocalDate.now().compareTo(endTime);
-////        if (isActive && )
-//    }
 }
