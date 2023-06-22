42ad29e9b78fe80aaa00e6c4506e5b161849e91a
Tikhomi <tt.vv.aa.l00@gmail.com>
2023-06-22 12:25:49 +0400

    added settings in application.properties related to sending mail

--- src/main/java/com/example/LibraryService/service/EmailSenderServiceImpl.java
+++ src/main/java/com/example/LibraryService/service/EmailSenderServiceImpl.java
@@ -1,19 +1,23 @@
 package com.example.LibraryService.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSenderServiceImpl implements EmailSenderService {
+    @Value("${email.sender}")
+    private String emailSender;
+
     @Autowired
     private JavaMailSender javaMailSender;
 
     @Override
     public void sendEmail(String to, String subject, String message) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("47watron@rambler.ru"); // Установите свой адрес электронной почты
+        simpleMailMessage.setFrom(emailSender);
         simpleMailMessage.setTo(to);
         simpleMailMessage.setSubject(subject);
         simpleMailMessage.setText(message);
