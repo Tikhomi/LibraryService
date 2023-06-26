a3938c940d15a7562f715f06fec199da19ba448f
Yura <yura104bk@gmail.com>
2023-06-26 15:00:01 +0400

    Исправлен checkstyle

--- src/main/java/com/example/LibraryService/email/EmailSenderServiceImpl.java
+++ src/main/java/com/example/LibraryService/email/EmailSenderServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.LibraryService.email;
 
-import com.example.LibraryService.email.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
--- src/main/java/com/example/LibraryService/report/ReportController.java
+++ src/main/java/com/example/LibraryService/report/ReportController.java
@@ -1,6 +1,5 @@
 package com.example.LibraryService.report;
 
-import com.example.LibraryService.report.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
